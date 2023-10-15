bcec673e1445957e72050a8eb076860a16b41f23
m-d-nabeel <asharnabeel137@gmail.com>
2023-10-15 14:10:19 +0530

    added additional configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "green-light": "var(--green-light)",
+        "green-deep": "var(--green-deep)",
+        "green-yellow": "var(--green-yellow)",
+      },
+    },
   },
   plugins: [],
-}
+};
