762ac6801d6c2e50022d6959181e6894e43e369f
saniaahmad6 <sania.ahmad483@gmail.com>
2023-10-24 15:23:09 +0530

    people section updated - 2

---
+++ data/faculty_data.js
@@ -0,0 +1,159 @@
+
+export const faculty_data = [{
+    "name": "Prof. Bashir Alam",
+    "dp": "https://www.jmi.ac.in/upload/employee/balam220210804205147_l.jpg",
+    "onclick": "id3",
+    "designation": "(Professor & Head of Department )",
+    "department": "Department of Computer Engineering, Jamia Millia Islamia<br>New Delhi, Delhi-110025 India",
+    "mobileNumber": "not available",
+    "email": "balam2@jmi.ac.in , babashirala,@gmail.com",
+    "portfolio": "Professional Portfolio",
+    "areaOfIntrest": "Parallel Programming, Cluster programming, Distributed and Cloud computing, Information Security, High Performance Computing, GPU Computing",
+}, {
+    "name": "Dr. Mohammad Najmud Doja",
+    "dp": "https://www.jmi.ac.in/upload/employee/dojace_20100906095212_l.jpg",
+    "onclick": 'id1',
+    "designation": "(Director of I.I.I.T. Sonepat (On deputation))",
+    "department": "Department of Computer Engineering, Jamia Millia Islamia <br>                                                            New Delhi, Delhi-110025 India",
+    "mobileNumber": "+919811380489",
+    "email": "mdoja2jmi.ac.in, mndoja@gmail.com, ndoja@yahoo.com",
+    "portfolio": "Professional Portfolio",
+    "areaOfIntrest": "Software Engineering, Computer Networks, Security, Simulation, Soft Computing.",
+
+}, {
+    "name": "Prof. Tanvir Ahmad",
+    "dp": "html/educat-preview/educat/img/team/1.jpg",
+    "onclick": "id2",
+    "designation": "( Professor & Hony. Addl. Director : Website & MIS)",
+    "department": "Department of Computer Engineering, Jamia Millia Islamia <br>                                                            New Delhi, Delhi-110025 India",
+    "mobileNumber": "011-26980281",
+    "email": "tahmad2@jmi.ac.in, tanvir.jmi@gmail.com",
+    "portfolio": "Professional Portfolio",
+    "areaOfIntrest": "Text Mining, Graph Mining, Bigdata Analytics, Natural Language Processing.",
+
+}, {
+    "name": "Prof. Mohammad Amjad",
+    "dp": "https://www.jmi.ac.in/upload/employee/mamjad20141017124119_l.jpg",
+    "onclick": "id7",
+    "designation": "( Professor )",
+    "department": "Department of Computer Engineering, Jamia Millia Islamia <br>                                                            New Delhi, Delhi-110025 India",
+    "mobileNumber": "not available",
+    "email": "mamjad@jmi.ac.in",
+    "portfolio": "Professional Portfolio",
+    "areaOfIntrest": "Mobile Computing, Wireless Sensor Networks, Security in Cloud Computing",
+
+}, {
+    "name": "Dr. Sarfaraz Massod",
+    "dp": "https://www.jmi.ac.in/upload/employee/smasood20210729101539_l.png",
+    "onclick": "id11",
+    "designation": "( Assistant Professor )",
+    "department": "Department of Computer Engineering, Jamia Millia Islamia <br>                                                            New Delhi, Delhi-110025 India",
+    "mobileNumber": "+919654774604",
+    "email": "smasood@jmi.ac.in, sarfarazmasood2002@yahoo.com",
+    "portfolio": "Professional Portfolio",
+    "areaOfIntrest": "Soft Computing, Artificial Neural Networks, Image Processing",
+
+}, {
+    "name": "Dr. Mumtaz Ahmad",
+    "dp": "https://www.jmi.ac.in/upload/employee/mahmed120190904101826_l.jpg",
+    "onclick": "id10",
+    "designation": "( Assistant Professor )",
+    "department": "Department of Computer Engineering, Jamia Millia Islamia <br>                                                            New Delhi, Delhi-110025 India",
+    "mobileNumber": "011-26980281 , 9911669991",
+    "email": "ahmedmumtaz01@jmi.ac.in, mahmed1.jmi@gmail.com",
+    "portfolio": "Professional Portfolio",
+    "areaOfIntrest": "Computer Networks, Artificial Intelligence",
+
+}, {
+    "name": "Dr. Musheer Ahmad",
+    "dp": "images/musheerAhmad.png",
+    "onclick": "id9",
+    "designation": "( Assistant Professor )",
+    "department": "Department of Computer Engineering, Jamia Millia Islamia <br>                                                            New Delhi, Delhi-110025 India",
+    "mobileNumber": "+911126980281",
+    "email": "mahmad9@jmi.ac.in, musheer.cse@gmail.com",
+    "portfolio": "Professional Portfolio",
+    "areaOfIntrest": "Chaos-based Cryptography, Multimedia Security, Cryptanalysis, Image Processing",
+
+}, {
+    "name": "Dr. Mohammad Zeeshan Ansari",
+    "dp": "https://www.jmi.ac.in/upload/employee/mzansari20120919175515_l.jpg",
+    "onclick": "id8",
+    "designation": "( Assistant Professor )",
+    "department": "Department of Computer Engineering, Jamia Millia Islamia <br>                                                            New Delhi, Delhi-110025 India",
+    "mobileNumber": "9999395201,+91-1126980281",
+    "email": "mzansari@jmi.ac.in, zeeshan.niec@gmail.com",
+    "portfolio": "Professional Portfolio",
+    "areaOfIntrest": "Information Extraction, Text Mining, Machine Learning, Soft Computing",
+
+}, {
+    "name": "Dr Faiyaz Ahmad",
+    "dp": "https://www.jmi.ac.in/upload/employee/fahmad120160516102806_l.jpg",
+    "onclick": "id5",
+    "designation": "( Assistant Professor )",
+    "department": "Department of Computer Engineering, Jamia Millia Islamia <br>                                                            New Delhi, Delhi-110025 India",
+    "mobileNumber": "9999370237",
+    "email": "fahmad1@jmi.ac.in, ahmad.faiyaz@gmail.com",
+    "portfolio": "Professional Portfolio",
+    "areaOfIntrest": "Operating System, Image Processing, Big Data Mining",
+
+}, {
+    "name": "Dr. Danish Raza Rizvi",
+    "dp": "images/danishRizvi.png",
+    "onclick": "id4",
+    "designation": "( Assistant Professor )",
+    "department": "Department of Computer Engineering, Jamia Millia Islamia <br>                                                            New Delhi, Delhi-110025 India",
+    "mobileNumber": "011-26980281 , 9910625221",
+    "email": "drizvi@jmi.ac.in, Danish.jmi@gmail.com",
+    "portfolio": "Professional Portfolio",
+    "areaOfIntrest": "Wireless Sensor Networks Security, Mobile Computing, ADBMS",
+
+},  {
+    "name": "Mr. Jawahar Lal",
+    "dp": "images/jawaharLal.png",
+    "onclick": "id6",
+    "designation": "( Assistant Professor )",
+    "department": "Department of Computer Engineering, Jamia Millia Islamia <br>                                                            New Delhi, Delhi-110025 India",
+    "mobileNumber": "011-26980281",
+    "email": "jlal@jmi.ac.in, jawahar.jmi@gmail.com",
+    "areaOfIntrest": "Software Engineering and Testing, Artificial Intelligence, Operating System, Computer Networks",
+
+},{
+    "name": "Dr. Shahzad Alam",
+    "dp": "images/shahzadAlam.png",
+    "onclick": "id12",
+    "designation": "( Assistant Professor )",
+    "department": "Department of Computer Engineering, Jamia Millia Islamia <br>                                                            New Delhi, Delhi-110025 India",
+    "mobileNumber": "+919818538198",
+    "email": "salam7@jmi.ac.in (Primary), shahzad5alam@gmail.com (Secondary)",
+    "portfolio": "Professional Portfolio",
+    "areaOfIntrest": "Image processing, Information Security, Machine Learning, Cryptography and Soft Computing Techniques,Machine Learning",
+
+}, {
+    "name": "Mr. Shamim Ahmad",
+    "dp": 'images/shamimAhmad.png',
+    "onclick": "id13",
+    "designation": "( Assistant Professor - Contractual )",
+    "department": "Department of Computer Engineering, Jamia Millia Islamia <br>                                                            New Delhi, Delhi-110025 India",
+    "mobileNumber": "",
+    "areaOfIntrest": "Artificial Intelligence, IOT"
+
+}, {
+    "name": "Mr. Hannan Mansoor",
+    "dp": "images/hannanMansoor.png",
+    "onclick": "id14",
+    "designation": "( Assistant Professor - Contractual )",
+    "department": "Department of Computer Engineering, Jamia Millia Islamia <br>                                                            New Delhi, Delhi-110025 India",
+    "mobileNumber": "",
+    "areaOfIntrest": "Machine Learning, Natural Language Processing, Image Processing"
+
+}, {
+    "name": "Dr. Waseem Ahmed",
+    "dp": "images/waseemAhmed.png",
+    "onclick": "id15",
+    "designation": "( Assistant Professor - Contractual )",
+    "department": "Department of Computer Engineering, Jamia Millia Islamia <br>                                                            New Delhi, Delhi-110025 India",
+    "mobileNumber": "",
+    "areaOfIntrest": "Fuzzy Theory, Soft Computing, Data Mining"
+
+}]
