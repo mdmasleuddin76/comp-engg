1f93254d75f4ac733293767fd4b789362b5dadef
saniaahmad6 <sania.ahmad483@gmail.com>
2023-11-11 20:52:33 +0530

    projects page added

---
+++ data/ProjectData.js
@@ -0,0 +1,420 @@
+export const project_data = {
+    "major1": [
+      {
+        "ID": 1,
+        "group_number": "1.",
+        "supervisor_name": "Mr. Faiyaz Ahmad",
+        "project_title": "Water Management system using IoT",
+        "roll_numbers": [
+          "16BCS020",
+          "16BCS077"
+        ],
+        "names": [
+          "Faria Mabood",
+          "Abdul Irshad"
+        ]
+      }
+    ],
+    "major2": [
+      {
+        "ID": 2,
+        "group_number": "2.",
+        "supervisor_name": "Mrs. Some Supervisor",
+        "project_title": "Smart Home Automation",
+        "roll_numbers": [
+          "16BCS001",
+          "16BCS045"
+        ],
+        "names": [
+          "John Doe",
+          "Jane Smith"
+        ]
+      },
+      {
+        "ID": 3,
+        "group_number": "2.",
+        "supervisor_name": "Mrs. Some Supervisor",
+        "project_title": "Environmental Monitoring System",
+        "roll_numbers": [
+          "16BCS012",
+          "16BCS055"
+        ],
+        "names": [
+          "Alice Johnson",
+          "Bob Williams"
+        ]
+      }
+    ],
+    "minor1": [
+      {
+        "ID": 4,
+        "group_number": "3.",
+        "supervisor_name": "Mr. Another Supervisor",
+        "project_title": "Minor Project 1",
+        "roll_numbers": [
+          "16BCS003",
+          "16BCS066"
+        ],
+        "names": [
+          "Eva Anderson",
+          "Daniel Brown"
+        ]
+      }
+    ],
+    "minor2": [
+      {
+        "ID": 5,
+        "group_number": "4.",
+        "supervisor_name": "Mrs. Different Supervisor",
+        "project_title": "Minor Project 2",
+        "roll_numbers": [
+          "16BCS011",
+          "16BCS022"
+        ],
+        "names": [
+          "Grace Miller",
+          "Henry Taylor"
+        ]
+      }
+    ],
+    "minor17": [
+      {
+          "ID": 1,
+          "group_number": "1.",
+          "supervisor_name": "Mr. Musheer Ahmad",
+          "project_title": "A Novel Data encryption scheme using DNA computing",
+          "roll_numbers": [
+              "14BCS0019","14BCS0047","14BCS0054"
+          ],
+          "names": [
+              "Tafseer Ahmad","Subia Ansari","Iram Ayyub"
+          ]
+      },
+      {
+          "ID": 2,
+          "group_number": "2.",
+          "supervisor_name": "Prof. Tanvir Ahmad",
+          "project_title": "Home Automation using ARDUINO",
+          "roll_numbers": [
+              "14BCS0062","14BCS0063"
+          ],
+          "names": [
+              "Mubashshir Khan","Murtaza Hasan"
+          ]
+      },
+      {
+          "ID": 3,
+          "group_number": "3.",
+          "supervisor_name": "Mr. Jawahar Lal",
+          "project_title": "Opinion Mining using Deep Neural Networks.",
+          "roll_numbers": [
+              "14BCS0004","14BCS0012"
+          ],
+          "names": [
+              "Mohd. Faizan Shahid","Arpit Singh"
+          ]
+      },
+      {
+          "ID": 4,
+          "group_number": "4.",
+          "supervisor_name": "Mr. Musheer Ahmad",
+          "project_title": "Fractional Order Chaotic System Based S Box Design",
+          "roll_numbers": [
+              "14BCS0020","14BCS0035"
+          ],
+          "names": [
+              "Vaibhav Singhal","Saurabh Sharma"
+          ]
+      },
+      {
+          "ID": 5,
+          "group_number": "5.",
+          "supervisor_name": "Mr. Jawahar Lal",
+          "project_title": "Speaker recognition system using cepstral analysis",
+          "roll_numbers": [
+              "14BCS0058","14BCS0061"
+          ],
+          "names": [
+              "Md. Amir NaseemZeeshan Siddiqui"
+          ]
+      },
+      {
+          "ID": 6,
+          "group_number": "6.",
+          "supervisor_name": "Mr. Musheer Ahmad",
+          "project_title": "Image Encryption based on Algebraic S-Box and Galios field",
+          "roll_numbers": [
+              "14BCS0036","14BCS0041","14BCS0050"
+          ],
+          "names": [
+              "Abdul Mohsin Siddiqui","Mohd. Zayan Alam","Zaki Mustafa Farooqui"
+          ]
+      },
+      {
+          "ID": 7,
+          "group_number": "7.",
+          "supervisor_name": "Mr. Danish Raza Rizvi",
+          "project_title": "Maternal Diabetic Analysis",
+          "roll_numbers": [
+              "14BCS0017","14BCS0046"
+          ],
+          "names": [
+              "Shubham Garg","Sanjay Kumar"
+          ]
+      },
+      {
+          "ID": 8,
+          "group_number": "8.",
+          "supervisor_name": "Mr. Sarfaraz Masood",
+          "project_title": "A project on Noise reduction on image using Morphological image cleaning",
+          "roll_numbers": [
+              "14BCS0002","14BCS0009"
+          ],
+          "names": [
+              "Joy Mukherjee","Shabnam Khatoon"
+          ]
+      },
+      {
+          "ID": 9,
+          "group_number": "9.",
+          "supervisor_name": "Mr. Mumtaz Ahmad",
+          "project_title": "Automatic Entity Recognition",
+          "roll_numbers": [
+              "14BCS0038","14BCS0052"
+          ],
+          "names": [
+              "Akshay Sharma","Azfar Sikander Zaidi"
+          ]
+      },
+      {
+          "ID": 10,
+          "group_number": "10.",
+          "supervisor_name": "Prof. Tanvir Ahmad",
+          "project_title": "Artistic image style transfer using deep neural networks",
+          "roll_numbers": [
+              "14BCS0023","14BCS0053","14BCS0065"
+          ],
+          "names": [
+              "Abhishek Bhat","tHarsh Vijay","Rohit Kumar Wasan"
+          ]
+      },
+      {
+          "ID": 11,
+          "group_number": "11.",
+          "supervisor_name": "Mr. Mumtaz Ahmad",
+          "project_title": "BOT-CHEF: Making a platform for cybersecurity research and detection of vulnerable target online using Shodan.",
+          "roll_numbers": [
+              "14BCS0056","14BCS0026","14BCS0030"
+          ],
+          "names": [
+              "Lakhita Bhatia","Dhruv Malik","Mohd. Usman Alvi"
+          ]
+      },
+      {
+          "ID": 12,
+          "group_number": "12.",
+          "supervisor_name": "Mr. Danish Raza Rizvi",
+          "project_title": "Detection and Drawing of basic geometric shapes in a hand drawn diagram image",
+          "roll_numbers": [
+              "14BCS0006","14BCS0010"
+          ],
+          "names": [
+              "Sahiba Khan","Alima Siddiqui"
+          ]
+      },
+      {
+          "ID": 13,
+          "group_number": "13.",
+          "supervisor_name": "Mr. Faiyaz Ahmad",
+          "project_title": "Smart home monitoring using a chat bot and raspberry Pi",
+          "roll_numbers": [
+              "14BCS0042","14BCS0067"
+          ],
+          "names": [
+              "Mohd. Saqib","Syed Faheel Ahmad"
+          ]
+      },
+      {
+          "ID": 14,
+          "group_number": "14.",
+          "supervisor_name": "Mr. Sarfaraz Masood",
+          "project_title": "Clustering music by Genres using supervised and Unsupervised Algorithms",
+          "roll_numbers": [
+              "14BCS0027","14BCS0034"
+          ],
+          "names": [
+              "Farah Naaz","Purnima Garg"
+          ]
+      },
+      {
+          "ID": 15,
+          "group_number": "15.",
+          "supervisor_name": "Mr. Sarfaraz Masood",
+          "project_title": "Framework of a clothing fashion recommendation system",
+          "roll_numbers": [
+              "14BCS0024","14BCS0033"
+          ],
+          "names": [
+              "Aisha Aijaz","Mohd. Wajid Zaman"
+          ]
+      },
+      {
+          "ID": 16,
+          "group_number": "16.",
+          "supervisor_name": "Dr. Mohd. Amjad",
+          "project_title": "Smart Irrigation System",
+          "roll_numbers": [
+              "14BCS0025","14BCS0028"
+          ],
+          "names": [
+              "Anas Akhtar","Gaurav Arora"
+          ]
+      },
+      {
+          "ID": 17,
+          "group_number": "17.",
+          "supervisor_name": "Dr. Mohd. Amjad",
+          "project_title": "Sign Language Interpreter",
+          "roll_numbers": [
+              "14BCS0007","14BCS0039","14BCS0045"
+          ],
+          "names": [
+              "Saif Rehman Nasir","Anish Sharma","Rahul Tuteja"
+          ]
+      },
+      {
+          "ID": 18,
+          "group_number": "18.",
+          "supervisor_name": "Dr. Bashir Alam",
+          "project_title": "Finger Print Authentication system",
+          "roll_numbers": [
+              "14BCS0055","14BCS0059"
+          ],
+          "names": [
+              "Khalid Hasan Rahmani","Mohd. Ariz Ansari"
+          ]
+      },
+      {
+          "ID": 19,
+          "group_number": "19.",
+          "supervisor_name": "Mr. Shahzad Alam",
+          "project_title": "Edge detection in medical images using quantum image enhancement and Shanon entropy",
+          "roll_numbers": [
+              "14BCS0011","14BCS0032"
+          ],
+          "names": [
+              "Aquib Jawed","Mohd. Suhail"
+          ]
+      },
+      {
+          "ID": 20,
+          "group_number": "20.",
+          "supervisor_name": "Mr. Hannan Mansoor",
+          "project_title": "Exploring CNN-RNN architectures for multilabel classification of the Amazon",
+          "roll_numbers": [
+              "14BCS0003","14BCS0013"
+          ],
+          "names": [
+              "Kunal Kaushik","Aviral Mudgal"
+          ]
+      },
+      {
+          "ID": 21,
+          "group_number": "21.",
+          "supervisor_name": "Dr. Bashir Alam",
+          "project_title": "Moodifier",
+          "roll_numbers": [
+              "14BCS0001","14BCS0040"
+          ],
+          "names": [
+              "","AbdullahGourav Suri"
+          ]
+      },
+      {
+          "ID": 22,
+          "group_number": "22.",
+          "supervisor_name": "Mr. Shamim Ahmad",
+          "project_title": "User categorization from stack overflow data using BIG data techniques",
+          "roll_numbers": [
+              "14BCS0022","14BCS0037"
+          ],
+          "names": [
+              "Abhishek Barnwal","Afzal Ahmad"
+          ]
+      },
+      {
+          "ID": 23,
+          "group_number": "23.",
+          "supervisor_name": "Prof. M. N. Doja",
+          "project_title": "A novel approach towards spam classification",
+          "roll_numbers": [
+              "14BCS0008","14BCS0018","14BCS0043","14BCS0048","14BCS0049"
+          ],
+          "names": [
+              "Saquib Ali Khan","Suhail Khan","Najeeb Khan","Yasir Bashir Mir","Zafrullah Mahmood"
+          ]
+      },
+      {
+          "ID": 24,
+          "group_number": "24.",
+          "supervisor_name": "Mr. Mohd. Zeeshan Ansari",
+          "project_title": "Part of speech tagging of Hindi- English Code Mixed Text",
+          "roll_numbers": [
+              "14BCS0014"
+          ],
+          "names": [
+              "KashanUddin Z. Khan"
+          ]
+      },
+      {
+          "ID": 25,
+          "group_number": "25.",
+          "supervisor_name": "Mr. Mohd. Zeeshan Ansari",
+          "project_title": "Amazon Review representative ranking",
+          "roll_numbers": [
+              "14BCS0015","14BCS0016","14BCS0068"
+          ],
+          "names": [
+              "Rishabn Jain","Saurabh Purohit","Shikhar Chaudhary"
+          ]
+      },
+      {
+          "ID": 26,
+          "group_number": "26.",
+          "supervisor_name": "Mr. Mohd. Zeeshan Ansari",
+          "project_title": "Predictive Air Pollution levels of a city (using machine learning)",
+          "roll_numbers": [
+              "14BCS0057","14BCS0066"
+          ],
+          "names": [
+              "Lokesh Kumar","Shabahat Jawed"
+          ]
+      },
+      {
+          "ID": 27,
+          "group_number": "27.",
+          "supervisor_name": "Mr. Faiyaz Ahmad",
+          "project_title": "Autonomous parking using deep reinforcement learning",
+          "roll_numbers": [
+              "14BCS0031","14BCS0051"
+          ],
+          "names": [
+              "Mohd. Huzaifa Faruqi","Ajitesh Kumar Rai"
+          ]
+      },
+      {
+          "ID": 28,
+          "group_number": "28.",
+          "supervisor_name": "Prof. Tanvir Ahmad",
+          "project_title": "Application of Block chain in Education Industry",
+          "roll_numbers": [
+              "14BCS0060"
+          ],
+          "names": [
+              "Mohd. Sadiq"
+          ]
+      }
+  ]
+    // Add more as needed
+  }
+  
--- src/PageGroup/Research/Project.jsx
+++ src/PageGroup/Research/Project.jsx
@@ -1,9 +1,145 @@
+import { useState, useEffect } from "react";
+import { project_data } from "../../../data/ProjectData";
+import image from "../About/assets/aboutjmi.jpeg";
+import HeroSection from "../About/Component/HeroSection";
 
-function Project() {
+const Project = () => {
+  const [selectedNumber, setSelectedNumber] = useState("1");
+  const [selectedCategory, setSelectedCategory] = useState("major");
+  const [displayCategory, setDisplayCategory] = useState("Major");
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const itemsPerPage = 10;
+
+  const [filteredData, setFilteredData] = useState([]);
+  const [paginatedData, setPaginatedData] = useState([]);
+
+  useEffect(() => {
+    const newData = filterData(selectedNumber, selectedCategory);
+    setFilteredData(newData);
+    setCurrentPage(1);
+  }, [selectedNumber, selectedCategory]);
+
+  useEffect(() => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    setPaginatedData(filteredData.slice(startIndex, endIndex));
+  }, [filteredData, currentPage]);
+
+  const filterData = (number, category) => {
+    const key = `${category}${number}`;
+    return project_data[key] || [];
+  };
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
   return (
     <div>
-      <h1>Project</h1>
+      <HeroSection heading={"Research Projects"} image={image} />
+      <div className="flex w-full flex-col items-center justify-center gap-y-8 font-[450]">
+        <div className="-mt-12 h-8 w-full backdrop-blur-[2px] " />
+        <h3 className="mb-5 px-6 text-3xl font-medium">
+          {" "}
+          <span className="text-green-deep">Projects</span> List
+        </h3>
+      </div>
+
+      <div className="flex justify-center">
+        <div className="flex flex-row gap-x-10 ">
+          <label className="bg-deep text-grey border-silver  w-64 border p-4 px-4 py-2">
+            Select Group Number:
+            <select
+              value={selectedNumber}
+              onChange={(e) => setSelectedNumber(e.target.value)}
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="17">17</option>
+              {/* Add more options as needed */}
+            </select>
+          </label>
+
+          <label className="bg-deep text-grey border-silver w-64 border p-4 px-4 py-2">
+            Select Category:
+            <select
+              value={selectedCategory}
+              onChange={(e) => {
+                setDisplayCategory(e.target.value.toUpperCase());
+                setSelectedCategory(e.target.value)}}
+            >
+              <option value="major">Major</option>
+              <option value="minor">Minor</option>
+            </select>
+          </label>
+        </div>
+      </div>
+
+      {/* Display */}
+      <div>
+        <h2 className=" m-7 rounded-sm bg-green-deep p-4 text-center text-2xl font-bold text-white">
+          {displayCategory} Projects of 20{selectedNumber}
+        </h2>
+        
+      </div>
+
+      <div className="bg-gray-100 p-8">
+      <table className="min-w-full bg-white border border-gray-300">
+        <thead>
+          <tr>
+            <th className="py-2 px-4 border-b">No.</th>
+            <th className="py-2 px-4 border-b">Project Name</th>
+            <th className="py-2 px-4 border-b">Supervisor</th>
+            <th className="py-2 px-4 border-b">Students</th>
+            <th className="py-2 px-4 border-b">Rollno</th>
+          </tr>
+        </thead>
+        <tbody>
+          {paginatedData.map((data, index) => (
+            <tr key={index}>
+              <td className="py-2 px-4 border-b">{index+1}</td>
+              <td className="py-2 px-4 border-b">{data.project_title}</td>
+              <td className="py-2 px-4 border-b">{data.supervisor_name}</td>
+              <td className="py-2 px-4 border-b">{data.names.join(", ")}{" "}</td>
+              <td className="py-2 px-4 border-b">{data.roll_numbers.join(", ")}{" "}</td>
+              
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+
+      <div>
+        <div className="flex justify-center">
+          <div className="flex flex-row gap-x-10 ">
+            <button
+              onClick={() => handlePageChange(currentPage - 1)}
+              disabled={currentPage === 1}
+              className="text-grey border-silver w-64 border bg-green-deep p-4 px-4 py-2 hover:bg-green-700"
+            >
+              Previous Page
+            </button>
+            <span className="bg-deep text-grey w-64 border px-4 py-2">
+              {" "}
+              Page {currentPage}{" "}
+            </span>
+            <button
+              onClick={() => handlePageChange(currentPage + 1)}
+              disabled={currentPage * itemsPerPage >= filteredData.length}
+              className="text-grey border-silver w-64 border bg-green-deep p-4 px-4 py-2 hover:bg-green-700"
+            >
+              Next Page
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div>
+      
+      </div>
     </div>
   );
-}
+};
+
 export default Project;
