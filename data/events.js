f6efb79e917ba4f355c8e0c53afc14e09b2df5a3
saniaahmad6 <sania.ahmad483@gmail.com>
2024-01-17 20:41:16 +0530

    changes to events cards

--- data/events.js
+++ data/events.js
@@ -43,7 +43,7 @@ const y2017 = [{
 }];
 
 const y2016= [{
-  image : "https://images.pexels.com/photos/8761330/pexels-photo-8761330.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  image : "https://csejmi.github.io/html/educat-preview/educat/img/event/e10.jpg",
   desc : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
   venue: "FET Auditorium",
   date: "17-10-2016",
@@ -86,7 +86,24 @@ const y2016= [{
   
 }];
 
+const y2018= [{
+  image : "https://csejmi.github.io/html/educat-preview/educat/img/event/e7.jpg",
+  desc : "CSI-JMI organized 'Open mic' , where anyone could showcase their knowledge/ experiences/ talent, and get a chance to express themselves, on a topic of their interests (cultural, technical, literary, political, etc.). It helped the students grow, analyze their strengths and weaknesses about public speaking, develop their personalities and enhance leadership qualities in them. ",
+  date: "21st, 22nd November, 2018",
+  venue: "Room No. 328,3rd Floor, FET",
+  tagline: "Open Mic Competition",
+  
+},{
+  image : "https://csejmi.github.io/html/educat-preview/educat/img/event/e9.jpg",
+  desc : "CSI-JMI in collaboration with Skill Campus brought one of a kind work shop on App Development, on 26th October, 2018. iOS App Development is amongst the most popular skills in the industry presently. iOS apps are developed with Swift and objective C. The workshop was successful with the huge response received from the students. ",
+  venue: "Room No. 328,3rd Floor, FET",
+  date: "21st, 22nd November, 2018",
+  tagline: "iOS App Development",
+  
+},];
+
 export const data= {
     "2017" : y2017,
     "2016" : y2016,
+    "2018" : y2018
 }
