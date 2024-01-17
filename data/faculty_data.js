ccdbb09667c49077285eec779fc47d803e7a38a4
budhil <budhilnigam@gmail.com>
2024-01-17 22:36:43 +0530

    pdf changes

--- data/faculty_data.js
+++ data/faculty_data.js
@@ -13,7 +13,7 @@ export const HOD = {
     "mobileNumber": "not available",
     "email": "balam2@jmi.ac.in , babashirala,@gmail.com",
     "portfolio": "Professional Portfolio",
-    "pdf" :pdf,
+    "pdf" :"https://www.jmi.ac.in/upload/employeeresume/balam2.pdf",
     "areaOfIntrest": "Parallel Programming, Cluster programming, Distributed and Cloud computing, Information Security, High Performance Computing, GPU Computing",
     "details" : "Dr. Bashir Alam is currently Associate Professor in the Department of Computer Engineering, Faculty of Engineering and Technology, Jamia Millia Islamia (A Central University), New Delhi-110025. He has more than 15 years of teaching experience at post graduate, graduate and under graduate level. He received his B.Tech.(Computer Engineering) from Aligarh Muslim University Aligarh, Uttar Pradesh, India in 2000 with division First(Hons.)(8.6 /10 CPI), M.Tech. (IT) from IP University, Delhi, India in 2006 with division first (73 % marks), Ph.D.(Computer Engineering) from Jamia Millia Islamia, New Delhi, India in 2010. He previously worked as Lecturer, Department of Computer Science, Jamia Hamdard, New Delhi-110062 from 04/07/2000 to 21/09/2006. He was Principal Investigator for the following Funded Research Projects 'GPU based Router using Multigraph, funded by Jamia Millia Islamia, amount 1 lakh. “NVIDIA CUDA teaching Centre” funded by NVIDIA by means of donating one Tesla K20, two Quadro K5000 and two GTX 750 GPU along with two jetson TK1 Board and Books worth 6 Lakhs. A Major Research Project funded by Ministry of IT and Communication, government of India, amount 39.30 Lakhs (recommended by working group, Sanction letter is awaited). Prof Bashir Alam has published a total of 48 papers in which 22 of them are in journals, 24 of them are in International conferences and 2 are in national conference. He has also done Short term Course on High Performance Parallel and Enterprise Computing, April 12 – 18 2010, I.I.T. Kharagpur, India. Prof Bashir Alam has got CCNA and CCAI certification. He is a life time member of ISTE (Indian Society for Technical Education). He is Member of Board of Studies and faculty committee, Department of Computer Engineering, Jamia Millia Islamia, New Delhi. He was also Member of Board of Studies and faculty committee, Department of Computer Science, Jamia Hamdard, New Delhi. Prof Bashir Alam was also coach of Jamia Hamdard team for the 2003 ACM Asia ICPC(International Collegiate Programming Contest) at IIT Roorkee (10th Place) Coach and coach of Jamia Millia Islamia team for the 2014 ACM Asia ICPC(International Collegiate• Programming Contest) at IIIT Gwalior. He is also member of organizing team of Algorythm 2007 and Algorythm 2008 of department of Computer Engineering, Jamia Millia Islamia, New Delhi."
 
@@ -39,7 +39,7 @@ export const faculty_data = [{
     "mobileNumber": "011-26980281",
     "email": "tahmad2@jmi.ac.in, tanvir.jmi@gmail.com",
     "portfolio": "Professional Portfolio",
-    "pdf" :pdf,
+    "pdf" :"https://www.jmi.ac.in/upload/employeeresume/tahmad2.pdf",
     "areaOfIntrest": "Text Mining, Graph Mining, Bigdata Analytics, Natural Language Processing.",
     "details" : "Dr. Tanvir Ahmad is currently in the Department of Computer Engineering, Faculty of Engineering and Technology, Jamia Millia Islamia, New Delhi-110025. He has been involved with Department of Computer Engineering, Jamia Millia Islamia for over 13 years. He received his B.Sc. (Computer Sc.) from Bangalore University with First Class, M.Tech (I.T). from I.P. University, New Delhi in 2006 and Ph.D. on “Frequent and Sequential Pattern Mining and their Applications”, from Jamia Millia Islamia ,New Delhi in 2013.He has more than twenty years of academic, research, training and administrative experiences in the field of Computer Engineering. His areas of research are Text Mining, Graph Mining, Big Data Analytics, Natural Language Processing, Information Security. Prof. Tanvir Ahmad is an active researcher. He has more than 100 citations in referred journals, more that 20 papers indexed in SCOPUS and more that 26 papers indexed in Google scholar. Several students have been awarded PhD under his able guidance and several students are working under his supervision. He is Reviewer for Springer Journal for Neural Computing and Application. He member of Advisory Group for Digital Locker system, National e-Governance Division, Ministry of I.T. and Communication, Govt. of India. He is also member of Expert Committee of National Board of Accreditation (NBA) and of Expert Committee for affiliation to colleges for M.Tech, B.Tech and MCA under G.G.S.I.P. University since 2010. Prof Tanvir Ahmad is Hony. Additional Director of, FTK-Centre for Information Technology, for Website and MIS , Jamia Millia Islamia, New Delhi. He has worked as Head of Department of Computer Engineering from February, 2014 to February 2017. He is also a Member of Faculty Committee and Member of Board of Studies of Department of Computer Engineering, Jamia Millia Islamia. He was also the Chairman of Departmental Research Committee from Feb, 2014 to Feb, 2017 and President of Subject Association from Feb, 2014 to Feb, 2017. He is Chairman, of Syllabus Committee for drafting the course structure of M.Tech. in Computer Engineering. He is Teacher placement co-ordinator for Department of Computer Engineering from last 8 years. He is a Member of Jamia Teachers Association and Member of Sub-purchase committee of Department of Computer Engineering M.Tech (Computer Engineering), B.Tech (Computer Engineering), B.E. (Computer Engineering), M.C.A., M.Sc (Computer Science), M.Sc (Industrial Maths with Computer Application), B.C.A., Diploma Engineering."
 
@@ -52,7 +52,7 @@ export const faculty_data = [{
     "department": "Department of Computer Engineering, Jamia Millia Islamia <br>                                                            New Delhi, Delhi-110025 India",
     "mobileNumber": "not available",
     "email": "mamjad@jmi.ac.in",
-    "pdf" :pdf,
+    "pdf" :"https://www.jmi.ac.in/upload/employeeresume/mamjad.pdf",
     "portfolio": "Professional Portfolio",
     "areaOfIntrest": "Mobile Computing, Wireless Sensor Networks, Security in Cloud Computing",
     "details" : "Prof. Mohammad Amjad has obtained his B.Tech. in Computer Engineering from Aligarh Muslim University Aligarh, India in 1997 with first class. He obtained his M.Tech. (Information Technology) degree from IP University New Delhi, India in 2008 with first class and Ph.D. (Computer Engineering) degree from Jamia Millia Islamia in the year 2013, New Delhi India. He joined the Crescent computer Pvt. Ltd. New Delhi as a senior Network engineer in 1997. He was having the responsibilities to look after the Networks Design of Northern Indian Railway, Telecommunication of India Limited (TCIL) and Videsh Sanchar Nigam Limited (VSNL). Thereafter he joined as a Lecturer in the Department of Computer Science and Information Technology in MJP Rohilkhand University Bareilly U.P. In the year 2002 , he joined the Department of Computer Engineering University polytechnic, F/o Engineering and Technology Jamia Millia Islamia and then after he joined the Department of Computer Engineering F/o Engineering & Technology, Jamia Millia Islamia New Delhi in 2006 . He has the teaching experience of approximately 13 Years. Dr. Amjad is Assistant Professor in the Department of Computer Engineering, Faculty of Engineering & Technology of Jamia Millia Islamia, New Delhi. Dr. Amjad has earned the Ph.D. degree for his research work in the field of Mobile Ad hoc Networks, where he investigated the clustering in MANET and Energy consumption in Wireless sensor Networks and designed a “Quality of Service Framework for Mobile Ad hoc Networks”. Dr. Amjad’s research work has been published in refereed International journals/ International conferences of repute. He contributed 15 research papers during his Ph.D. to the IEEE explore/ Springer/ International Journals in U.S.A/ International Conferences etc. He is actively involved in research and development activities in areas of MANET, WSN, Green Computing and Network Security systems. Dr. Amjad is having the life membership of ISTE and an expert lecturer on the topics related to Green cloud Computing, Wireless sensor Networks, Mobile Communication, Network security and Mobile Ad hoc Networks."
@@ -62,7 +62,7 @@ export const faculty_data = [{
     "dp": "https://mdmshf.github.io/assets/images/11.jpg",
     "onclick": "id11",
     "designation": "( Assistant Professor )",
-    "pdf" :pdf,
+    "pdf" :"https://www.jmi.ac.in/upload/employeeresume/smasood.pdf",
     "department": "Department of Computer Engineering, Jamia Millia Islamia <br>                                                            New Delhi, Delhi-110025 India",
     "mobileNumber": "+919654774604",
     "email": "smasood@jmi.ac.in, sarfarazmasood2002@yahoo.com",
@@ -81,7 +81,7 @@ export const faculty_data = [{
     "email": "ahmedmumtaz01@jmi.ac.in, mahmed1.jmi@gmail.com",
     "portfolio": "Professional Portfolio",
     "areaOfIntrest": "Computer Networks, Artificial Intelligence",
-    "pdf" :pdf,
+    "pdf" :"https://www.jmi.ac.in/upload/employeeresume/mahmed1.pdf",
     "details" : "Dr. Mumtaz Ahmed is currently Assistant Professor in the Department of Computer Engineering, Faculty of Engineering and Technology, Jamia Millia Islamia, New Delhi-110025. He has more than 9 years of teaching experiences in the field of Computer Engineering. He is pursuing his Ph.D from Jamia Millia Islamia, New Delhi, had received his M.TECH from Delhi College Of Engineering, New Delhi in 2013 and B.TECH from Jamia Millia Islamia in 2004. Dr. Mumtaz Ahmed has published a total of 7 papers , 2 of which are in National/International Conferences and rest are in National/International Journals. He has also taken part in various Workshop/Conferences including ISTE workshop on Database Management System conducted by IIT-Bombay fomr 13th to 23rd Dec 2010. His research interest includes Computer Networks, Artificial Intelligence and Operating Systems"
 
 }, {
@@ -94,7 +94,7 @@ export const faculty_data = [{
     "email": "mahmad9@jmi.ac.in, musheer.cse@gmail.com",
     "portfolio": "Professional Portfolio",
     "areaOfIntrest": "Chaos-based Cryptography, Multimedia Security, Cryptanalysis, Image Processing",
-    "pdf" :pdf,
+    "pdf" :"https://www.jmi.ac.in/upload/employeeresume/mahmad9.pdf",
     "details" : "He has more than 8 years of teaching experiences in the field of Computer Engineering. He received his M.TECH in Computer Science & Engineering from ZH College of Engineering and Technology, Aligarh Muslim University in 2004 and B.TECH in Computer Engineering from ZH College of Engineering and Technology, Aligarh Muslim University in 2004. His M.Tech Dissertation topic was “Design and implementation of LFSR based Data Encryption Circuit for Secure Communications”. His research interest includes : Chaos-based Security Multidisciplinary Cryptography Cryptanalysis Information Hiding Secure Sequence Design Hash Function Design Digital Image Processing. Dr. Musheer Ahmad has published a total of 10 papers in journals and 32 Conference papers. He is currently supervising 3 B.Tech projectsand had supervised a total of 23 B.Tech final year project and 9 M.Tech projects. He is Editorial Board member of International Journal of Chaos, Control, Modelling and Simulation(IJCCMS) and The Journal of Theoretical Physics and Cryptography(IJTPC). He is also Technical Program Committee member of over 16 International conferences and Journal and Reviewer of over 34 International conferences and Journal. Dr. Musheer Ahmad has also Prepared Department’s Report for NAAC Accreditation and serving as Nodal faculty member for NAAC related activities. He is also member of Society of Digital Information and Wireless Communication(SDIWC), USA, Member of International Association of Computer Science and Information Technology (IACSIT), Singapore and Member of Institute of Doctors, Engineers and Scientists (IDES), India"
 
 }, {
@@ -107,7 +107,7 @@ export const faculty_data = [{
     "email": "mzansari@jmi.ac.in, zeeshan.niec@gmail.com",
     "portfolio": "Professional Portfolio",
     "areaOfIntrest": "Information Extraction, Text Mining, Machine Learning, Soft Computing",
-    "pdf" :pdf,
+    "pdf" :"https://www.jmi.ac.in/upload/employeeresume/mzansari.pdf",
     "details" : "Dr. Mohd Zeeshan Ansari is currently Assistant Professor at Department of Computer Engineering, Jamia Millia Islamia (A Central University), New Delhi. He received M.Tech in Computer Science and Engineering from Delhi Technological University, New Delhi and B.Tech in Computer Science and Engineering from Uttar Pradesh Technical University, Lucknow, Uttar Pradesh. He has more than nine years of teaching experience. His area of research interest is Code Mixing, Information Extraction and Retrieval, Text Mining, Natural Language Processing and Soft Computing Techniques. His field of Specialization is Information Extraction and Retrieval. He has taken part in many workshops/seminars like summer special programme organized by UGC-Academic Staff College and GIAN Course on Fairness accuracy and transparency in Machine Learning. He is also member of various organizations like IDES, IRED, IAENG, IIJRC. He has also published two chapters in journals and books namely A simple and efficient S-box design using Fisher Yates shuffle technique and Enhancement of Fuzzy Rank Aggregation Technique. He is also member of Departmental Committee for Curriculum Development of M.Tech, Computer Science and Engineering and NBA Coordinator of Dept of Computer Engineering. He is attendance coordinator of computer engineering department."
 
 
@@ -121,7 +121,7 @@ export const faculty_data = [{
     "email": "fahmad1@jmi.ac.in, ahmad.faiyaz@gmail.com",
     "portfolio": "Professional Portfolio",
     "areaOfIntrest": "Operating System, Image Processing, Big Data Mining",
-    "pdf" :pdf,
+    "pdf" :"https://www.jmi.ac.in/upload/employeeresume/fahmad1.pdf",
     "details" : "Dr. Faiyaz Ahmad is currently Assistant Professor in the Department of Computer Engineering, Faculty of Engineering and Technology, Jamia Millia Islamia, New Delhi-110025. He has more than 9 years of academic experiences in the field of Computer Engineering . He is pursuing his PhD in Computer Engineering from Department of Computer Engineering, Jamia Millia Islamia New Delhi. He did his M.Tech(IT) from Guru Gobind Singh Indraprastha University, Delhi with first division. And B.Tech. In Computer Engineering from Dr. Bhim Rao Ambedkar University (Formerly Known as Agra University) with Hons. His area of interest includes Operating System, Image Processing, Big data, Data Mining and has published 14 journals till now and had taken part in various seminars and conferences. He is Member of Faculty Committee and board of studies, Faculty of Engineering & Technology Jamia Millia Islamia. Question Paper Setter/ Examiner of Various Courses of several University. He is Member of Jamia Teachers Association and of Jamia Cultural Committee"
 
 
@@ -135,7 +135,7 @@ export const faculty_data = [{
     "email": "drizvi@jmi.ac.in, Danish.jmi@gmail.com",
     "portfolio": "Professional Portfolio",
     "areaOfIntrest": "Wireless Sensor Networks Security, Mobile Computing, ADBMS",
-    "pdf" :pdf,
+    "pdf" :"https://www.jmi.ac.in/upload/employeeresume/drizvi.pdf",
     "details" : "Dr. Danish Raza Rizvi is currently Assistant Professor in the Department of Computer Engineering, Faculty of Engineering and Technology, Jamia Millia Islamia, New Delhi-110025. He has more than 10 years of teaching experiences in the field of Computer Engineering and Information Technology. He is currently doing his Ph.D from Jamia Millia Islamia, he received his M.TECH in Information Technology from USIT, GGS Indraprastha University, New Delhi in 2008 and B.TECH in Computer Science & Engineering from M.J.P. Rohilkhand University, Bareilly, U.P. in 2003. His M.TECH dissertation topic was Data Security and Authentication using Steganography. Dr. Danish Raza Rizvi has published a total of 7 papers in journals. He is currently supervising 5 B.Tech projects and 3 M.Tech projects and had supervised a total of 21 B.Tech final year project and 15 final year Diploma engineering projects (2004 – 2010). His research interest includes Data Security, Network Security and Steganography"
 },  {
     "name": "Mr. Jawahar Lal",
@@ -183,7 +183,7 @@ export const faculty_data = [{
     "department": "Department of Computer Engineering, Jamia Millia Islamia <br>                                                            New Delhi, Delhi-110025 India",
     "mobileNumber": "",
     "areaOfIntrest": "Machine Learning, Natural Language Processing, Image Processing",
-    "pdf" :pdf,
+    "pdf" :"https://www.jmi.ac.in/upload/employeeresume/hannan_mansoor_ce.pdf",
     "details" : ""
 
 
@@ -195,7 +195,7 @@ export const faculty_data = [{
     "department": "Department of Computer Engineering, Jamia Millia Islamia <br>                                                            New Delhi, Delhi-110025 India",
     "mobileNumber": "",
     "areaOfIntrest": "Fuzzy Theory, Soft Computing, Data Mining",
-    "pdf" :pdf,
+    "pdf" :"https://www.jmi.ac.in/upload/employeeresume/waseem_ahmed_ce.pdf",
     "details" : ""
 
 
