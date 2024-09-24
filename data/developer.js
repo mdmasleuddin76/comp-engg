b1ce14789b0c8a5b593a8e72ac439427e3aa70b8
Insharah Ayyubi <weharrypotter@gmail.com>
2024-09-24 14:29:15 +0530

    minor project 21-25 added

--- data/developer.js
+++ data/developer.js
@@ -1,5 +1,4 @@
-import shahzadSirImg from "../src/PageGroup/Placement/assets/shahzadSir.jpeg";
-import zeeshanSirImg from "../src/PageGroup/People/assets/zeeshanSir.jpeg";
+import MumtazSirImg from '../src/PageGroup/People/assets/MumtazSir.png';
 import amjadSirImg from "../src/PageGroup/People/assets/amjadSir.png";
 import masleuddin from "../src/PageGroup/People/assets/Developer/masleuddin.jpg";
 import insharahImg from "../src/PageGroup/Placement/assets/insharah.jpeg";
@@ -15,25 +14,18 @@ import nabeel from"../src/PageGroup/People/assets/Developer/nabeel.jpeg";
 
 export const faculty = [
   {
-    name: "Prof. Mohd Amjad (Professor)",
-    designation: "(Coordinator)  Hony. Adll. Director:Website & MIS",
+    name: "Prof. Mohd Amjad (HoD)",
+    designation: "Coordinator",
     img: amjadSirImg,
     mail: "mailto:mamjad@jmi.ac.in",
     linkedin: "https://www.linkedin.com"
   },
   {
-    name: "Dr. Mohd Zeeshan Ansari (Assistant Professor)",
-    designation: "(Counsellor)",
-    img: zeeshanSirImg,
-    mail: "mailto:mzansari@jmi.ac.in",
-    linkedin: "https://www.linkedin.com/in/ansarimz"
-  },
-  {
-    name: "Dr. Shahzad Alam (Assistant Professor)",
-    designation: "(Counsellor)",
-    img: shahzadSirImg,
-    mail: "mailto:salam7@jmi.ac.in",
-    linkedin: "https://www.linkedin.com/in/dr-shahzad-alam"
+    name: "Dr Mumtaz Ahmed (Associate Professor)",
+    designation: "Counsellor",
+    img: MumtazSirImg,
+    mail: "mailto:mahmed1@jmi.ac.in",
+    linkedin: "https://www.linkedin.com/in/mumtaz-ahmed"
   },
 ];
 
